04f2e6061f9aeeea73cb16d267fe1a08e214746d
Meenatechjays <meena.sivakumar@techjays.com>
2025-08-24 23:52:43 +0530

    hero section created

--- responsive-typography/src/App.tsx
+++ responsive-typography/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import { HeroComponent } from './module/hero_section/HeroComponent'
 import { HeadingPage } from './pages/HeadingPage'
+import { LandingPage } from './pages/landingPage/LandingPage'
 
 function App() {
 
 
   return (
     <>
-     <HeadingPage/>
+     {/* <HeadingPage/> */}
+     {/* <LandingPage/> */}
+     <HeroComponent/>
     </>
   )
 }
