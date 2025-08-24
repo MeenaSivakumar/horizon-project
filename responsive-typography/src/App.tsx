5509c1119b8dacb1f31482d4708222c8bd7bfe71
Meenatechjays <meena.sivakumar@techjays.com>
2025-08-24 13:19:05 +0530

    headers with fluid typography

---
+++ responsive-typography/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { HeadingPage } from './pages/HeadingPage'
+
+function App() {
+
+
+  return (
+    <>
+     <HeadingPage/>
+    </>
+  )
+}
+
+export default App
---
+++ responsive-typography/src/pages/HeadingPage.tsx
@@ -0,0 +1,13 @@
+import styles from './HeadingPage.module.css';
+
+export const HeadingPage = ({})=>{
+    return(
+        <>
+        
+         <h1 className={styles.h1}>Welcome To My webPage</h1>
+      <h2 className={styles.h2}>Realtive Units</h2>
+      <h3 className={styles.h3}>Fluid Typography</h3>
+      <h4 className={styles.h4}> Responsive Design</h4>
+      </>
+    )
+}
